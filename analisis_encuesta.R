2880013bc0acf4720773520ca49dab3b8cf7c8a9
AnVales <a.vales@outlook.es>
2021-11-24 15:33:23 +0100

    Add files via upload

--- analisis_encuesta.R
+++ analisis_encuesta.R
@@ -24,6 +24,9 @@ row_names <- rownames(data)
 # Quantile
 quantile_values <- c()
 iqr_values <- c()
+mean_values <- c()
+sd_values <- c()
+var_values <- c()
 
 for (i in 1:5){
   
@@ -32,39 +35,60 @@ for (i in 1:5){
   
   # Interquartile Range
   iqr_values <- append(iqr_values, IQR(unlist(data[i])))
-}
-
-# Outlayers
-for (i in 1:5) {
-  boxplot.stats(unlist(data[i]))$out
   
-}
-
-# Standard deviation 
-sd_values <- c()
-
-for (i in 1:5){
-  column_sd <- sd(unlist(data[i]))
-  sd_values <- append(sd_values, column_sd) 
-}
-
-# Variation
-var_values <- c()
-
-for (i in 1:5){
-  column_var <- var(unlist(data[i]))
-  var_values <- append(var_values, column_var) 
+  # Mean
+  mean_values <- append(mean_values, mean(unlist(data[i])))
+  
+  # Standard deviation 
+  sd_values <- append(sd_values, sd(unlist(data[i])))
+  
+  # Variation
+  var_values <- append(var_values, var(unlist(data[i])))
+  
 }
 
 # Boxplot
 boxplot_overview <- boxplot(data, xlab = "Adjetivos", ylab = "Valoración", col = rgb(1, 0, 0, alpha = 0.4),border = "black", outpch = 25)
 
-# Histogram
-hist(x = data$Pesima...Excelente, xlab = " Pésima - Excelente", ylab = "Frecuencia", main = "")
-hist(x = data$Nula...Preparados, xlab = " Nula - Preparados", ylab = "Frecuencia", main = "")
-hist(x = data$Inservible...Servible, xlab = " Inservible - Servible", ylab = "Frecuencia", main = "")
-hist(x = data$Complicado...Funcional, xlab = " Complicado - Funcional", ylab = "Frecuencia", main = "")
-hist(x = data$ï..Baja...Alta, xlab = " Baja - Alta", ylab = "Frecuencia", main = "")
-
 # t de student
-t.test(data)
+Media <- 0
+t.test(data$Pesima...Excelente, mu = Media)
+
+# Barplot with error bars
+
+# Load ggplot2
+library(ggplot2)
+names <-  c("Baja-Alta", " Pésima-Excelente", "Nula-Preparados", "Inservible-Servible", "Complicado-Funcional")
+
+df <- data.frame(
+  name=names,
+  value=mean_values,
+  sd=sd_values
+)
+
+dev.new(width=7, height=7, unit="in")
+
+ggplot(df) +
+  geom_bar( aes(x=name, y=value), stat="identity", fill="mediumpurple1", alpha=0.7) +
+  geom_errorbar( aes(x=name, ymin=value-sd, ymax=value+sd), width=0.25, colour="orange", alpha=0.8, size=0.9)+
+  labs(title="¿Cómo evaluaría las competencias digitales de los docentes?", x ="Adjetivos", y = "Valoraciones") +
+  scale_y_continuous(breaks = c(-3, -2, -1, 0, 1, 2, 3), limits = c(-3, 3))+ 
+  theme(
+    text = element_text(size=15),
+    plot.title = element_text(color="black", size=18, face="bold", hjust = 0.5),
+    axis.title.x = element_text(color="gray16", size=16, face="bold"),
+    axis.title.y = element_text(color="gray16", size=16, face="bold"),
+    panel.background = element_rect(fill = "gray90",
+                                    colour = "gray90",
+                                    size = 0.9, linetype = "solid")
+  )
+
+
+# ANOVA
+library(psych)
+describe.by(data)
+
+library(rapportools)
+
+
+
